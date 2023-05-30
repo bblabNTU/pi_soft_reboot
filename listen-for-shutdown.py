2cb3bbb94c4e4054e823cda25dbf2be431610cd9
Hao-Ping <haopingchen1999@gmail.com>
2023-05-30 12:16:20 +0800

    add comment in listen-for-shutdown.py

--- listen-for-shutdown.py
+++ listen-for-shutdown.py
@@ -4,11 +4,12 @@ import subprocess
 from time import sleep
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(13, GPIO.FALLING)
-sleep(3)
-if GPIO.input(13) ==0:
-    #still pressed, shutdown pi
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP) #pull up to high
+GPIO.wait_for_edge(13, GPIO.FALLING) # when the GPIO is pulled down to GND
+sleep(3) # wait to determine long press or short press
+if GPIO.input(13) ==0: 
+    # after 3 seconds if it is still pulled down to GND, shutdown
     subprocess.call(['shutdown', '-h', 'now'], shell=False)
 else:
+    # else short press for reboot
     subprocess.call(['shutdown', '-r', 'now'], shell=False)
